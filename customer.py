62e2550ecbc9305bd16d9ce4b6762f754c99cc4a
SamCode28 <sglowe28@gmail.com>
2025-03-27 13:38:43 -0500

    Added methods from bank class for better organization

--- customer.py
+++ customer.py
@@ -13,11 +13,41 @@ class Customer:
     def deposit(self, amount : float):
         self.balance += amount
 
-    def withdrawal(self, amount :float):
-        self.balance -= amount
+    def withdrawal(self):
+        while(True):
+            try:
+                amount = float(input("How much would you like to withdraw?"))
 
-        if(self.balance < 0 and amount > 0):
-            self.balance -= 100
+            except ValueError:
+                print("Please enter a digit amount. ")
+                continue
+
+            else:
+                resulting_balance = self.balance - amount
+
+                if(resulting_balance < 0):
+                    self.overdraft_warning(resulting_balance)
+
+                else:
+                    self.balance -= amount
+                    print(f"You have removed {amount} from your account. \n")
+                return
+
+    def overdraft_warning(self, resulting_balance : float):
+        print(f"\nWarning: This withdrawal will place your account in a negative balance, ${resulting_balance}, which will cause an automatic overdraft fee of $100.")
+        while(True):
+            try:
+                answer = input("Would you like to continue? Y/N ").upper()
+            except ValueError:
+                print("Please enter 'Y' to continue or 'N' to stop transaction")
+                continue
+            else:
+                if(answer[0] == "Y"):
+                    self.balance = resulting_balance - 100
+                    print(f"Your new balance is {self.balance}\n")
+                else:
+                    print("Transaction cancelled.\n")
+                return
         
     def __str__(self):
         return f"Your balance is ${self.balance}"
