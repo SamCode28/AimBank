5946d3977c816bce2f33919890c9becc75ff2f8c
SamCode28 <sglowe28@gmail.com>
2025-03-31 18:32:42 -0500

    Added email support

--- aim_bank.py
+++ aim_bank.py
@@ -137,9 +137,67 @@ class StateBank:
 
         #doc.add_paragraph('TEST item in unordered list', style='List Bullet')
         doc.save(f'{customer.f_name}_{customer.l_name}_Bank_Statment.docx')
+        
     
     def email_statement(self):
+        self.generate_statement()
+        self.attach_print_statement()
+        self.send_email(self.generate_email())
         print(f"An email has been sent to {self.customer_logged_in.email}.\n")
+
+    def generate_email(self):
+        customer = self.customer_logged_in
+        base_template = f"""<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {{
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            color: #333;
+            background-color: #f9f9f9;
+            padding: 20px;
+        }}
+        .header {{
+            background-color: #0078D7; /* Microsoft blue */
+            color: white;
+            padding: 10px;
+            text-align: center;
+            border-radius: 5px;
+        }}
+        .content {{
+            margin: 20px 0;
+        }}
+        a {{
+            color: #0078D7;
+            text-decoration: none;
+        }}
+        a:hover {{
+            text-decoration: underline;
+        }}
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h1>{customer.f_name} {customer.l_name} Bank Statement</h1>
+    </div>
+    <div class="content">
+        <p>Hello, {customer.f_name} {customer.l_name}.</p>
+        <p>Attached you will find the total history of your deposits, withdrawals, and any potential fees your account may have incurred.</p>
+        <p>If you have any other questions, please email support@statebank.com.</p>
+        
+        <p>Warm regards,</p>
+        <p>{self.name}</p>
+    </div>
+</body>
+</html>"""
+        return base_template
+        
+    def attach_print_statement(self):
+        pass
+
+    def send_email(self, email : str):
+        print(email)
             
     def sign_in(self):
         attempts = 5
@@ -240,7 +298,7 @@ Password: """)
             if(password == password_confirm):
                 new_user = Customer(f_name, l_name, username, password)
                 self.bank_users.update({new_user.f_name + " " + new_user.l_name : new_user})
-                print(f"\nAccount succesfully created!  Welcome to State Bank, {f_name} {l_name}!  To access your bank, please log in.\n")
+                print(f"\nAccount succesfully created!  Welcome to State Bank, {f_name} {l_name}!  The username, {new_user.username}, has been created for you.  To access your bank, please log in.\n")
                 return
             
             #Send back to create a password
